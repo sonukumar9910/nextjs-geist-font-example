254cf914d4c985a34728c62df3f2510570ae78d6
sonukumar9910 <91356276+sonukumar9910@users.noreply.github.com>
2025-06-16 12:04:59 +0530

    Add Controllers/HomeController.cs 1750055699455

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageSlider.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
